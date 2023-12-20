268812a237e809a6c150dea184824e574083cf37
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-12-20 15:09:27 +0900

    initial commit

---
+++ src/main/java/org/tenten/tentenbe/TenTenBeApplication.java
@@ -0,0 +1,13 @@
+package org.tenten.tentenbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenTenBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TenTenBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tenten/tentenbe/TenTenBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tenten.tentenbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenTenBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
