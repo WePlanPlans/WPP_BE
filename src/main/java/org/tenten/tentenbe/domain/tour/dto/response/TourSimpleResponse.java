af0bfcca5193879b5ab4036de9e9667a78554833
Wonbn <wwscan1@naver.com>
2023-12-27 00:12:28 +0900

    Docs: Tour API 스웨거 상세작성

--- src/main/java/org/tenten/tentenbe/domain/tour/dto/response/TourSimpleResponse.java
+++ src/main/java/org/tenten/tentenbe/domain/tour/dto/response/TourSimpleResponse.java
@@ -1,4 +1,28 @@
 package org.tenten.tentenbe.domain.tour.dto.response;
 
-public record TourSimpleResponse() {
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record TourSimpleResponse(
+    @Schema(defaultValue = "1")
+    Long id,
+
+    @Schema(defaultValue = "여행지 이름")
+    String title,
+
+    @Schema(defaultValue = "4.5")
+    Double rating,
+
+    @Schema(defaultValue = "100")
+    Long reviewCount,
+
+    @Schema(defaultValue = "100")
+    Long goodCount,
+
+    @Schema(defaultValue = "false")
+    Boolean good,
+
+    @Schema(defaultValue = "http://~~~~~~image.jpg")
+    String smallThumbnailUrl
+) {
+
 }
