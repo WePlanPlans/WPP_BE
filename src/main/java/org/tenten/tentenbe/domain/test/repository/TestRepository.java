ab1be7782356fca68a46a74a41b5da168e436628
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-12-27 22:36:11 +0900

    Bugfix : deploy recovery 시간 300초로 설정

--- src/main/java/org/tenten/tentenbe/domain/test/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package org.tenten.tentenbe.domain.test.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.tenten.tentenbe.domain.test.entity.Test;
-
-public interface TestRepository extends JpaRepository<Test, Long> {
-}
