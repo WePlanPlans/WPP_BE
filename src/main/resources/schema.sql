e878f0959eec8ebf576ea9b9ce44b43dbc78e4f8
Wonbn <wwscan1@naver.com>
2023-12-28 19:09:11 +0900

    Refactor: sql 파일 수정
    - TourItemReviewKeyword.sql 데이터 줄바꿈
    - schema.sql 누락된 Column 추가 및 Column 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -161,11 +161,14 @@ create table Trip
     numberOfPeople bigint       null,
     tripId         bigint auto_increment
         primary key,
+    departure      varchar(255)                    null,
+    destination      varchar(255)                    null,
     tripName       varchar(255)                    null,
-    isDeleted      boolean                      null,
+    isDeleted      boolean                         null,
     tripStatus     enum ('BEFORE', 'AFTER', 'ING') null,
     area           varchar(255)                    null,
-    subarea        varchar(255)                    null
+    subarea        varchar(255)                    null,
+    budget         bigint                          null
 );
 
 create table TripItem
