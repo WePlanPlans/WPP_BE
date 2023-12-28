ed8378bdbcec5f9e2b297f5c1ab1211e03618a9f
Wonbn <wwscan1@naver.com>
2023-12-28 15:07:25 +0900

    Refactor: sql 파일 수정
    - 제목에 있는 [한국관광 품질인증/Korea Quality] 문구 제거
    - mapX -> longitude / mapY -> latitude
    - keyWord -> keyword

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -72,8 +72,8 @@ create table TourItem
         primary key,
     address              varchar(255) null,
     detailedAddress      varchar(255) null,
-    longitude                 varchar(255) null,
-    latitude                 varchar(255) null,
+    longitude            varchar(255) null,
+    latitude             varchar(255) null,
     originalThumbnailUrl varchar(255) null,
     smallThumbnailUrl    varchar(255) null,
     tel                  varchar(255) null,
@@ -131,7 +131,7 @@ create table ReviewKeyword
     reviewKeywordId bigint auto_increment
         primary key,
     constraint FKa4v4wydktt3nkb8k3yq2fj9d2
-        foreign key (keywordId) references KeyWord (keywordId),
+        foreign key (keywordId) references Keyword (keywordId),
     constraint FKip2fmgieccwcqg5lw59ds0l80
         foreign key (reviewId) references Review (reviewId)
 );
