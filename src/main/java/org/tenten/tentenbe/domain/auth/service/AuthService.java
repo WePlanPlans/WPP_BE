a3bc16a1ee11281f800d9a59c561d2c70591d94d
유현 <yh7119@gmail.com>
2023-12-30 04:23:04 +0900

    Feat: 로그인 기능 추가

--- src/main/java/org/tenten/tentenbe/domain/auth/service/AuthService.java
+++ src/main/java/org/tenten/tentenbe/domain/auth/service/AuthService.java
@@ -1,24 +1,36 @@
 package org.tenten.tentenbe.domain.auth.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.tenten.tentenbe.domain.auth.dto.request.LoginRequest;
 import org.tenten.tentenbe.domain.auth.dto.request.SignUpRequest;
 import org.tenten.tentenbe.domain.auth.dto.response.LoginResponse;
+import org.tenten.tentenbe.domain.auth.dto.response.MemberDto;
 import org.tenten.tentenbe.domain.auth.exception.MemberAlreadyExistException;
 import org.tenten.tentenbe.domain.member.model.Member;
 import org.tenten.tentenbe.domain.member.repository.MemberRepository;
+import org.tenten.tentenbe.domain.token.dto.TokenDTO;
+import org.tenten.tentenbe.global.security.jwt.JwtTokenProvider;
 
 import static org.tenten.tentenbe.global.common.enums.LoginType.EMAIL;
 import static org.tenten.tentenbe.global.common.enums.UserAuthority.ROLE_USER;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AuthService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final JwtTokenProvider jwtTokenProvider;
+//    private final RefreshTokenService refreshTokenService;
 
     @Transactional
     public void signUp(SignUpRequest signUpRequest) {
@@ -38,8 +50,28 @@ public class AuthService {
     };
 
     @Transactional
-    public LoginResponse login(Long memberId, LoginRequest loginRequest) {
-        return null;
+    public LoginResponse login(LoginRequest loginRequest) {
+
+        UsernamePasswordAuthenticationToken authenticationToken = loginRequest.toAuthentication();
+
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.getObject();
+        Authentication authenticate = authenticationManager.authenticate(authenticationToken);
+
+//        Authentication authenticate = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+        TokenDTO.TokenInfoDTO tokenInfoDTO = jwtTokenProvider.generateTokenDto(authenticate);
+        log.info("로그인 API 중 토큰 생성 로직 실행");
+
+        String userEmail = authenticate.getName();
+        Member user = getMemberByEmail(userEmail);
+
+//        String refreshToken = tokenInfoDTO.getRefreshToken();
+//        refreshTokenService.saveOrUpdate(user, refreshToken);
+
+        return LoginResponse.builder()
+            .memberDto(MemberDto.fromEntity(user))
+            .tokenInfo(tokenInfoDTO.toTokenIssueDTO())
+            .build();
     }
     @Transactional
     public LoginResponse loginKakao(Long memberId, LoginRequest loginRequest) {
