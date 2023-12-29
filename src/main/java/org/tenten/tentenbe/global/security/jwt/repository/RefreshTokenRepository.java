367b28bb94c6e09216ac9dc8afe8b0ba9d29eda9
유현 <yh7119@gmail.com>
2023-12-30 01:31:55 +0900

    Feat: findByMember_Email 메소드 추가

---
+++ src/main/java/org/tenten/tentenbe/global/security/jwt/repository/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package org.tenten.tentenbe.global.security.jwt.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.tenten.tentenbe.global.security.jwt.model.RefreshToken;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String refreshToken);
+    Optional<RefreshToken> findByMember_Email(String email);
+}
+
