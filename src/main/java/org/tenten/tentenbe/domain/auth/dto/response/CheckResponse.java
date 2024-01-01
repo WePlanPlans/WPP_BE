6801893989209c55749adb3829d1aa4b0cc3278e
유현 <yh7119@gmail.com>
2024-01-01 16:44:55 +0900

    Feat: 반환 목적으로 Builder 추가

--- src/main/java/org/tenten/tentenbe/domain/auth/dto/response/CheckResponse.java
+++ src/main/java/org/tenten/tentenbe/domain/auth/dto/response/CheckResponse.java
@@ -1,7 +1,9 @@
 package org.tenten.tentenbe.domain.auth.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
 
+@Builder
 public record CheckResponse(
     @Schema(description = "중복 여부", defaultValue = "true")
     boolean exists
