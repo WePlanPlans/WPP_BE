24d727a00544830e6f6c1745018fcb785117e0a7
유현 <yh7119@gmail.com>
2024-01-02 17:22:11 +0900

    Feat: 나의 관심 여행지 api Liked 도메인으로 변경 및 회원 정보 조회 기능 추가

--- src/main/java/org/tenten/tentenbe/domain/member/controller/MemberController.java
+++ src/main/java/org/tenten/tentenbe/domain/member/controller/MemberController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.tenten.tentenbe.domain.member.dto.request.MemberUpdateRequest;
+import org.tenten.tentenbe.domain.member.dto.response.MemberDetailResponse;
 import org.tenten.tentenbe.domain.member.dto.response.MemberResponse;
 import org.tenten.tentenbe.domain.member.service.MemberService;
 import org.tenten.tentenbe.domain.review.dto.response.ReviewInfo;
@@ -16,12 +17,11 @@ import org.tenten.tentenbe.domain.tour.dto.response.TourSimpleResponse;
 import org.tenten.tentenbe.domain.trip.dto.response.TripSimpleResponse;
 import org.tenten.tentenbe.global.response.GlobalDataResponse;
 import org.tenten.tentenbe.global.response.GlobalResponse;
-import org.tenten.tentenbe.global.util.SecurityUtil;
 
-import static io.swagger.v3.oas.annotations.enums.ParameterIn.PATH;
 import static io.swagger.v3.oas.annotations.enums.ParameterIn.QUERY;
 import static org.tenten.tentenbe.global.common.constant.ResponseConstant.DELETED;
 import static org.tenten.tentenbe.global.common.constant.ResponseConstant.SUCCESS;
+import static org.tenten.tentenbe.global.util.SecurityUtil.getCurrentMemberId;
 
 @Tag(name = "유저 관련 API", description = "유저 관련 API 모음입니다.")
 @RestController
@@ -54,16 +54,6 @@ public class MemberController {
         return ResponseEntity.ok(GlobalDataResponse.ok(SUCCESS, memberService.getTours(1L, PageRequest.of(page, size))));
     }
 
-    @Operation(summary = "나의 관심 여행지 삭제 API", description = "나의 관심 여행지 삭제 API 입니다.")
-    @DeleteMapping("/tour/{tourId}")
-    public ResponseEntity<GlobalResponse> deleteTour(
-        @Parameter(name = "tourId", description = "삭제할 여행지 아이디", in = PATH)
-        @PathVariable("tourId") Long tourId
-    ) {
-        memberService.deleteTour(null, tourId);
-        return ResponseEntity.ok(GlobalResponse.ok(DELETED));
-    }
-
     @Operation(summary = "나의 리뷰 조회 API", description = "나의 리뷰 조회 API 입니다.")
     @GetMapping("/reviews")
     public ResponseEntity<GlobalDataResponse<Page<ReviewInfo>>> getReviews(
@@ -78,8 +68,8 @@ public class MemberController {
 
     @Operation(summary = "회원 정보 조회 API", description = "회원 정보 조회 API 입니다.")
     @GetMapping()
-    public ResponseEntity<GlobalDataResponse<MemberResponse>> getMemberInfo() {
-        return ResponseEntity.ok(GlobalDataResponse.ok(SUCCESS, memberService.getMemberInfo(null)));
+    public ResponseEntity<GlobalDataResponse<MemberDetailResponse>> getMemberInfo() {
+        return ResponseEntity.ok(GlobalDataResponse.ok(SUCCESS, memberService.getMemberInfo(getCurrentMemberId())));
     }
 
     @Operation(summary = "회원 정보 수정 API", description = "회원 정보 수정 API 입니다.")
@@ -93,8 +83,7 @@ public class MemberController {
     @Operation(summary = "회원 탈퇴 API", description = "회원 탈퇴 API 입니다.")
     @DeleteMapping()
     public ResponseEntity<GlobalResponse> deleteMember() {
-        Long currentMemberId = SecurityUtil.getCurrentMemberId();
-        memberService.deleteMember(currentMemberId);
+        memberService.deleteMember(getCurrentMemberId());
         return ResponseEntity.ok(GlobalResponse.ok(DELETED));
     }
 }
