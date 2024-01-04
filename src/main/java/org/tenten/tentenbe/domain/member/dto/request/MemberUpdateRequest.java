a4dc0200669c3212c9cbad4b8b05913c61fff6c4
유현 <yh7119@gmail.com>
2024-01-04 23:01:21 +0900

    Feat: 프로필 업데이트 기능 추가

--- src/main/java/org/tenten/tentenbe/domain/member/dto/request/MemberUpdateRequest.java
+++ src/main/java/org/tenten/tentenbe/domain/member/dto/request/MemberUpdateRequest.java
@@ -2,13 +2,19 @@ package org.tenten.tentenbe.domain.member.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import org.tenten.tentenbe.domain.member.model.Survey;
+import org.tenten.tentenbe.global.common.enums.AgeType;
+import org.tenten.tentenbe.global.common.enums.GenderType;
 
 public record MemberUpdateRequest(
     @Schema(defaultValue = "닉네임")
     String nickname,
-    Survey survey,
     @Schema(defaultValue = "프로필 이미지")
     String profileImageUrl,
+    @Schema(defaultValue = "연령대")
+    AgeType ageType,
+    @Schema(defaultValue = "성별")
+    GenderType genderType,
+    Survey survey,
     @Schema(defaultValue = "as@#SD23/&DFd%fs@a1")
     String password
 ) {
