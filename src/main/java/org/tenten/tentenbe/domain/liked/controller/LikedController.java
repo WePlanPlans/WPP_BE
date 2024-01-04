8fba628061e8f258242d974f0f97a5bbef04b71b
유현 <yh7119@gmail.com>
2024-01-04 16:28:04 +0900

    Feat: 관심 여행지 등록, 취소 기능 추가

--- src/main/java/org/tenten/tentenbe/domain/liked/controller/LikedController.java
+++ src/main/java/org/tenten/tentenbe/domain/liked/controller/LikedController.java
@@ -2,18 +2,18 @@ package org.tenten.tentenbe.domain.liked.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.tenten.tentenbe.domain.liked.service.LikedService;
 import org.tenten.tentenbe.global.response.GlobalResponse;
 
+import static org.tenten.tentenbe.global.common.constant.ResponseConstant.DELETED;
 import static org.tenten.tentenbe.global.common.constant.ResponseConstant.SUCCESS;
 import static org.tenten.tentenbe.global.util.SecurityUtil.getCurrentMemberId;
 
+@Tag(name = "관심 여행지(좋아요) 관련 API", description = "관심 여행지 관련 API 모음입니다.")
 @RestController
 @RequestMapping("/api/liked")
 @RequiredArgsConstructor
@@ -21,13 +21,22 @@ public class LikedController {
 
     private final LikedService likedService;
 
-    @Operation(summary = "관심 여행지 등록/취소 API", description = "관심 여행지 등록/취소 API 입니다.")
+    @Operation(summary = "관심 여행지 등록 API", description = "관심 여행지 등록 API 입니다.")
     @PostMapping("/{tour_id}")
     public ResponseEntity<GlobalResponse> likeTour(
-        @Parameter(description = "관심 여행지 등록/취소할 여행지 ID", required = true, example = "1")
+        @Parameter(description = "관심 여행지 등록할 여행지 ID", required = true, example = "1")
         @PathVariable("tour_id") Long tourId) {
         likedService.likeTour(tourId, getCurrentMemberId());
         return ResponseEntity.ok(GlobalResponse.ok(SUCCESS));
+    }
+
+    @Operation(summary = "관심 여행지 삭제 API", description = "관심 여행지 삭제 API 입니다.")
+    @DeleteMapping("/{tour_id}")
+    public ResponseEntity<GlobalResponse> cancelLikeTour(
+        @Parameter(description = "관심 여행지 삭제할 여행지 ID", required = true, example = "1")
+        @PathVariable("tour_id") Long tourId) {
+        likedService.cancelLikeTour(tourId, getCurrentMemberId());
+        return ResponseEntity.ok(GlobalResponse.ok(DELETED));
     }
 
 }
