88e867487ee5ace0fd419bab81b0d1bbd0711131
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2024-01-10 00:28:08 +0900

    Refactor : 배포 서버 Trip 도메인 구조 변경 반영

--- src/main/java/org/tenten/tentenbe/domain/trip/model/Trip.java
+++ src/main/java/org/tenten/tentenbe/domain/trip/model/Trip.java
@@ -5,12 +5,15 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 import org.tenten.tentenbe.global.common.BaseTimeEntity;
 import org.tenten.tentenbe.global.common.enums.TripStatus;
+import org.tenten.tentenbe.global.converter.MapConverter;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import static jakarta.persistence.CascadeType.REMOVE;
 import static jakarta.persistence.EnumType.STRING;
@@ -39,7 +42,13 @@ public class Trip extends BaseTimeEntity {
     private Boolean isDeleted;
     private String tripName;
     private Long budget;
-
+    @ColumnDefault("0")
+    private Long tripItemPriceSum;
+    @ColumnDefault("0")
+    private Long transportationPriceSum;
+    @Convert(converter = MapConverter.class)
+    @Column(columnDefinition = "JSON")
+    private Map<String, Long> tripPathPriceMap;
     @OneToMany(mappedBy = "trip", fetch = LAZY, cascade = REMOVE)
     private final List<TripMember> tripMembers = new ArrayList<>();
 
