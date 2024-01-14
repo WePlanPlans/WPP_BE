c1df753b85ebb829f54f06899105704ff8d8318f
jun <jundev21@gmail.com>
2024-01-15 03:04:34 +0900

    FEAT : trip 테스트 케이스 작성

--- src/test/java/org/tenten/tentenbe/domain/trip/controller/TripControllerTest.java
+++ src/test/java/org/tenten/tentenbe/domain/trip/controller/TripControllerTest.java
@@ -1,12 +1,165 @@
 package org.tenten.tentenbe.domain.trip.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
 import org.tenten.tentenbe.common.ControllerTest;
+import org.tenten.tentenbe.config.WithMockCustomUser;
+import org.tenten.tentenbe.domain.trip.dto.response.TripCreateResponse;
+import org.tenten.tentenbe.domain.trip.dto.response.TripLikedSimpleResponse;
+import org.tenten.tentenbe.domain.trip.dto.response.TripSimpleResponse;
 import org.tenten.tentenbe.domain.trip.service.TripService;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.tenten.tentenbe.common.fixture.TripFixture.*;
 
 public class TripControllerTest extends ControllerTest {
 
-    @Autowired
+    @MockBean
     private TripService tripService;
+
+    @Test
+    @DisplayName("여정 생성 성공")
+    public void createTripSuccess() throws Exception {
+
+        given(tripService.createTrip(any(), any())).willReturn(new TripCreateResponse(1L));
+
+        mockMvc.perform(post("/api/trips")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(tripCreateRequest())))
+                .andExpect(jsonPath("$.data.tripId").value(1L))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("나의 여정목록 조회 성공")
+    public void getTripsSuccess() throws Exception {
+
+        Pageable pageable = PageRequest.of(0, 10);
+
+        List<TripSimpleResponse> tripSimpleResponseList = List.of(tripSimpleResponse());
+        Page<TripSimpleResponse> tripSimpleResponsePage = new PageImpl<>(tripSimpleResponseList, pageable, tripSimpleResponseList.size());
+
+        given(tripService.getTrips(any(), any())).willReturn(tripSimpleResponsePage);
+
+        mockMvc.perform(get("/api/trips"))
+                .andExpect(jsonPath("$.data.content.[0].tripId").value(1L))
+                .andExpect(jsonPath("$.data.content.[0].tripName").value("my first trip response"))
+                .andExpect(jsonPath("$.data.content.[0].numberOfTripMembers").value(4L))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("여정 상세조회 성공")
+    public void getDetailTripSuccess() throws Exception {
+
+        given(tripService.getTrip(any())).willReturn(tripDetailResponse());
+
+        mockMvc.perform(get("/api/trips/1"))
+                .andExpect(jsonPath("$.data.tripName").value("trip name test"))
+                .andExpect(jsonPath("$.data.startDate").value("2024-01-01"))
+                .andExpect(jsonPath("$.data.endDate").value("2024-01-06"))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("여정 정보 수정 성공")
+    public void updateTripSuccess() throws Exception {
+        given(tripService.updateTrip(any(), any(), any())).willReturn(tripInfoUpdateResponse());
+
+        mockMvc.perform(put("/api/trips/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(tripInfoUpdateRequest())))
+                .andExpect(jsonPath("$.data.tripName").value("my second trip response"))
+                .andExpect(jsonPath("$.data.startDate").value("2024-01-01"))
+                .andExpect(jsonPath("$.data.endDate").value("2024-01-06"))
+                .andExpect(jsonPath("$.data.area").value("서울시"))
+                .andExpect(jsonPath("$.data.subarea").value("강남구"))
+                .andDo(print());
+
+    }
+
+    @Test
+    @DisplayName("여정 탈퇴 성공")
+    public void deleteTripMemberSuccess() throws Exception {
+
+        //TODO:: 코드 완성
+        mockMvc.perform(delete("/api/trips/1"))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+    }
+
+    @Test
+    @DisplayName("그룹 관심 여행지 등록 성공")
+    public void LikeTourInOurTripSuccess() throws Exception {
+
+        mockMvc.perform(post("/api/trips/1/tripLikedTours")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(tripLikedItemRequest())))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("SUCCESS"))
+                .andDo(print());
+
+    }
+
+    @Test
+    @WithMockCustomUser
+    @DisplayName("그룹 관심목록 조회 성공")
+    public void getTripLikedItemsSuccess() throws Exception {
+
+        Pageable pageable = PageRequest.of(0, 10);
+
+        List<TripLikedSimpleResponse> tripLikedSimpleResponses = List.of(tripLikedSimpleResponse());
+        Page<TripLikedSimpleResponse> tripSimpleResponsePage = new PageImpl<>(tripLikedSimpleResponses, pageable, tripLikedSimpleResponses.size());
+
+        given(tripService.getTripLikedItems(any(), any(), any(), any())).willReturn(tripSimpleResponsePage);
+
+        mockMvc.perform(get("/api/trips/1/tripLikedTours"))
+                .andExpect(jsonPath("$.data.content.[0].tripLikedItemId").value(1L))
+                .andExpect(jsonPath("$.data.content.[0].ratingAverage").value(2.5))
+                .andExpect(jsonPath("$.data.content.[0].reviewCount").value(10L))
+                .andExpect(jsonPath("$.data.content.[0].notPreferTotalCount").value(3L))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("그룹 관심 여행지 좋아요 여부 확인 성공")
+    public void preferOrNotTourInOurTripSuccess() throws Exception {
+
+        mockMvc.perform(post("/api/trips/1/tripLikedTours/1?prefer=true&notPrefer=true"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("SUCCESS"))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("그룹 여행 취향 조회")
+    public void getTripSurveysSuccess() throws Exception {
+
+        given(tripService.getTripSurveys(any())).willReturn(tripSurveyResponse());
+
+        mockMvc.perform(get("/api/trips/1/survey"))
+                .andExpect(jsonPath("$.data.planningTotalCount").value(20L))
+                .andExpect(jsonPath("$.data.planningCount").value(10L))
+                .andExpect(jsonPath("$.data.activeHoursTotalCount").value(30L))
+                .andExpect(jsonPath("$.data.activeHoursCount").value(40L))
+                .andExpect(jsonPath("$.data.accommodationTotalCount").value(4L))
+                .andExpect(jsonPath("$.data.accommodationCount").value(2L))
+                .andDo(print());
+
+    }
 
 }
--- src/test/java/org/tenten/tentenbe/domain/trip/service/TripServiceTest.java
+++ src/test/java/org/tenten/tentenbe/domain/trip/service/TripServiceTest.java
@@ -1,6 +1,189 @@
 package org.tenten.tentenbe.domain.trip.service;
 
-import org.tenten.tentenbe.domain.member.service.MemberServiceTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.tenten.tentenbe.common.ServiceTest;
+import org.tenten.tentenbe.domain.member.repository.MemberRepository;
+import org.tenten.tentenbe.domain.tour.repository.TourItemRepository;
+import org.tenten.tentenbe.domain.trip.dto.response.*;
+import org.tenten.tentenbe.domain.trip.model.TripLikedItem;
+import org.tenten.tentenbe.domain.trip.model.TripLikedItemPreference;
+import org.tenten.tentenbe.domain.trip.repository.TripLikedItemPreferenceRepository;
+import org.tenten.tentenbe.domain.trip.repository.TripLikedItemRepository;
+import org.tenten.tentenbe.domain.trip.repository.TripMemberRepository;
+import org.tenten.tentenbe.domain.trip.repository.TripRepository;
 
-public class TripServiceTest extends MemberServiceTest {
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.tenten.tentenbe.common.fixture.AuthFixture.newBasicMember;
+import static org.tenten.tentenbe.common.fixture.MemberFixture.tourItem;
+import static org.tenten.tentenbe.common.fixture.TripFixture.*;
+
+public class TripServiceTest extends ServiceTest {
+
+    @InjectMocks
+    private TripService tripService;
+    @Mock
+    private TripRepository tripRepository;
+    @Mock
+    private TourItemRepository tourItemRepository;
+    @Mock
+    private MemberRepository memberRepository;
+    @Mock
+    private  TripMemberRepository tripMemberRepository;
+    @Mock
+    private  TripLikedItemRepository tripLikedItemRepository;
+    @Mock
+    private  TripLikedItemPreferenceRepository tripLikedItemPreferenceRepository;
+
+    @Test
+    @DisplayName("여정 생성이 가능하다.")
+    public void createTripSuccess(){
+
+        given(tripMemberRepository.countTripMemberByMember(any())).willReturn(1L);
+        given(tripMemberRepository.save(any())).willReturn(savedTripMember(newBasicMember(),trip()));
+        given(tripRepository.save(any())).willReturn(trip());
+
+        TripCreateResponse result = tripService.createTrip(newBasicMember().getId(), tripCreateRequest());
+
+        assertNotNull(result);
+        assertThat(result.tripId()).isEqualTo(10L);
+    }
+
+    @Test
+    @DisplayName("여정 조회가 가능하다.")
+    public void getTripsSuccess(){
+
+        Pageable pageable = PageRequest.of(0, 10);
+
+        List<TripSimpleResponse> tripSimpleResponseList = List.of(tripSimpleResponse());
+        Page<TripSimpleResponse> tripSimpleResponsePage = new PageImpl<>(tripSimpleResponseList, pageable, tripSimpleResponseList.size());
+
+        given(tripRepository.findTripsByMemberId(any(),any())).willReturn(tripSimpleResponsePage);
+
+        Page<TripSimpleResponse> result = tripService.getTrips(newBasicMember().getId(), pageable);
+
+        assertNotNull(result);
+
+        assertThat(result.getContent()).usingRecursiveAssertion().isEqualTo(tripSimpleResponsePage.getContent());
+    }
+
+    @Test
+    @DisplayName("상세 여정 조회가 가능하다.")
+    public void getTripSuccess(){
+
+        given(tripRepository.findById(any())).willReturn(Optional.ofNullable(trip()));
+
+        TripDetailResponse result = tripService.getTrip(trip().getId());
+
+        assertNotNull(result);
+        assertThat(result.tripName()).isEqualTo(trip().getTripName());
+        assertThat(result.startDate()).isEqualTo(trip().getStartDate());
+        assertThat(result.endDate()).isEqualTo(trip().getEndDate());
+    }
+
+    @Test
+    @DisplayName("여정 업데이트가 가능하다.")
+    public void updateTripSuccess(){
+
+        given(memberRepository.getReferenceById(any())).willReturn(newBasicMember());
+        given(tripRepository.findById(any())).willReturn(Optional.ofNullable(trip()));
+        given(tripRepository.save(any())).willReturn(trip());
+
+        TripInfoUpdateResponse result = tripService.updateTrip(newBasicMember().getId(), trip().getId(),tripInfoUpdateRequest());
+
+        assertNotNull(result);
+        assertThat(result.tripName()).isEqualTo(tripInfoUpdateRequest().tripName());
+    }
+
+    @Test
+    @DisplayName("여정 멤버 삭제가 가능하다.")
+    public void deleteTripMemberSuccess(){
+        //TODO:: 구현 구상 후 작성 예정
+    }
+
+    @Test
+    @DisplayName("그룹 관심 여행지 등록 이 가능하다.")
+    public void LikeTourInOurTripSuccess(){
+
+        given(memberRepository.getReferenceById(any())).willReturn(newBasicMember());
+        given(tripRepository.findById(any())).willReturn(Optional.ofNullable(trip()));
+        given(tourItemRepository.findById(any())).willReturn(Optional.ofNullable(tourItem()));
+        given(tripLikedItemRepository.findByTripAndTourItem(any(),any()))
+                .willReturn(Optional.empty());
+
+        tripService.LikeTourInOurTrip(newBasicMember().getId(), trip().getId(), tripLikedItemRequest());
+
+        verify(tripLikedItemRepository, times(4)).save(any(TripLikedItem.class));
+
+    }
+
+    @Test
+    @DisplayName("그룹 관심 목록 조회가 가능하다. ")
+    public void getTripLikedItemsSuccess(){
+
+        Pageable pageable = PageRequest.of(0, 10);
+
+        List<TripLikedItem> tripLikedItems = List.of(saveTripLikedItem(trip(),tourItem()));
+        Page<TripLikedItem> tripLikedItemPage = new PageImpl<>(tripLikedItems, pageable, tripLikedItems.size());
+
+
+        given(memberRepository.getReferenceById(any())).willReturn(newBasicMember());
+        given(tripRepository.findById(any())).willReturn(Optional.ofNullable(trip()));
+        given(tripLikedItemRepository.findTripLikedItemsById(any(), any(), any(), any()))
+                .willReturn(new PageImpl<>(List.of(tripLikedSimpleResponse()), pageable, tripLikedItems.size()));
+
+        Page<TripLikedSimpleResponse> result = tripService.getTripLikedItems(newBasicMember().getId(), trip().getId(), "관광지", pageable);
+
+        assertNotNull(result);
+
+        assertThat(result.getContent()).usingRecursiveComparison()
+                .isEqualTo(List.of(tripLikedSimpleResponse()));
+
+    }
+
+    @Test
+    @DisplayName("그룹 관심 여행지 좋아요 여부 확인 이 가능하다.")
+    public void preferOrNotTourInOurTripSuccess(){
+
+        given(tripRepository.findById(any())).willReturn(Optional.ofNullable(trip()));
+        given(tourItemRepository.findById(any())).willReturn(Optional.ofNullable(tourItem()));
+        given(tripMemberRepository.findByMemberAndTrip(any(),any())).willReturn(Optional.ofNullable(savedTripMember(newBasicMember(),trip())));
+        given(tripLikedItemRepository.findByTripAndTourItem(any(), any()))
+                .willReturn(Optional.ofNullable(saveTripLikedItem(trip(), tourItem())));
+//        given(tripLikedItemPreferenceRepository.findByTripMemberAndTripLikedItem(savedTripMember(), saveTripLikedItem(trip(), tourItem())))
+//                .willReturn(Optional.ofNullable(tripLikedItemPreference()));
+
+        tripService.preferOrNotTourInOurTrip(newBasicMember().getId(), trip().getId(), tourItem().getId(), false, true);
+
+        verify(tripLikedItemPreferenceRepository).save(any(TripLikedItemPreference.class));
+
+    }
+
+    @Test
+    @DisplayName("그룹 여행 취향 조회가 가능하다.")
+    public void getTripSurveysSuccess(){
+
+        //TODO:: 코드 보완필
+        given(tripRepository.findById(any())).willReturn(Optional.ofNullable(trip()));
+//        given(tripMemberRepository.findByMemberAndTrip(any(),any())).willReturn(Optional.ofNullable(savedTripMember()));
+
+        TripSurveyResponse result = tripService.getTripSurveys(trip().getId());
+
+        assertThat(result.planningTotalCount()).isEqualTo(0);
+
+    }
 }
