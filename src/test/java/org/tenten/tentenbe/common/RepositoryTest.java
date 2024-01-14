a4eb3ef3bf3497bf3b8402a465b590a411e9a732
jun <jundev21@gmail.com>
2024-01-15 03:03:01 +0900

    FEAT : repository 부모 클래스 작성

--- src/test/java/org/tenten/tentenbe/common/RepositoryTest.java
+++ src/test/java/org/tenten/tentenbe/common/RepositoryTest.java
@@ -10,8 +10,11 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.tenten.tentenbe.domain.liked.repository.LikedItemRepository;
 import org.tenten.tentenbe.domain.member.repository.MemberRepository;
+import org.tenten.tentenbe.domain.review.repository.KeywordRepository;
+import org.tenten.tentenbe.domain.review.repository.ReviewKeywordRepository;
 import org.tenten.tentenbe.domain.review.repository.ReviewRepository;
 import org.tenten.tentenbe.domain.tour.repository.TourItemRepository;
+import org.tenten.tentenbe.domain.trip.repository.*;
 
 @DataJpaTest
 @ActiveProfiles("test")
@@ -26,16 +29,35 @@ public class RepositoryTest {
     protected LikedItemRepository likedItemRepository;
     @Autowired
     protected ReviewRepository reviewRepository;
+    @Autowired
+    protected TripRepository tripRepository;
+    @Autowired
+    protected TripItemRepository tripItemRepository;
+    @Autowired
+    protected TripMemberRepository tripMemberRepository;
+    @Autowired
+    protected TripLikedItemRepository tripLikedItemRepository;
+    @Autowired
+    protected TripLikedItemPreferenceRepository tripLikedItemPreferenceRepository;
+    @Autowired
+    protected ReviewKeywordRepository reviewKeywordRepository;
+    @Autowired
+    protected KeywordRepository keywordRepository;
     @PersistenceContext
     protected EntityManager entityManager;
     @BeforeEach
     protected void init() {
         entityManager.flush();
-        System.out.println("flushed data");
+        System.out.println("flushed data success");
         tourItemRepository.deleteAll();
         memberRepository.deleteAll();
         likedItemRepository.deleteAll();
+        tripRepository.deleteAll();
+        tripItemRepository.deleteAll();
         reviewRepository.deleteAll();
+        tripMemberRepository.deleteAll();
+        tripLikedItemRepository.deleteAll();
+        tripLikedItemPreferenceRepository.deleteAll();
         System.out.println("reset success");
     }
 
