b503c3c4cfc1065723a854fdfeead782c5038939
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2024-01-16 19:36:36 +0900

    Refactor : TripItem transportation 제거

--- src/main/java/org/tenten/tentenbe/domain/trip/model/TripItem.java
+++ src/main/java/org/tenten/tentenbe/domain/trip/model/TripItem.java
@@ -7,11 +7,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.tenten.tentenbe.domain.tour.model.TourItem;
 import org.tenten.tentenbe.global.common.BaseTimeEntity;
-import org.tenten.tentenbe.global.common.enums.Transportation;
 
 import java.time.LocalDate;
 
-import static jakarta.persistence.EnumType.STRING;
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static jakarta.persistence.InheritanceType.JOINED;
 
@@ -26,8 +24,6 @@ public class TripItem extends BaseTimeEntity {
     @GeneratedValue(strategy = IDENTITY)
     @Column(name = "tripItemId")
     private Long id;
-    @Enumerated(STRING)
-    private Transportation transportation;
     private Long seqNum; // 방문 순서
     private LocalDate visitDate;
     private Long price; // 예상 비용
