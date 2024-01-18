28f014b857c5d00c2c4be59ed8fae883d56d5d4a
yuhyun1 <yh7119@gmail.com>
2024-01-18 18:44:31 +0900

    Refactor: 쿼리 메서드를 이용한 가독성 향상 및 성능 최적화

--- src/main/java/org/tenten/tentenbe/domain/liked/repository/LikedItemRepository.java
+++ src/main/java/org/tenten/tentenbe/domain/liked/repository/LikedItemRepository.java
@@ -3,8 +3,11 @@ package org.tenten.tentenbe.domain.liked.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.tenten.tentenbe.domain.liked.model.LikedItem;
 import org.tenten.tentenbe.domain.member.model.Member;
+import org.tenten.tentenbe.domain.tour.dto.response.TourSimpleResponse;
 import org.tenten.tentenbe.domain.tour.model.TourItem;
 
 import java.util.Optional;
@@ -12,9 +15,29 @@ import java.util.Optional;
 public interface LikedItemRepository extends JpaRepository<LikedItem, Long> {
     Optional<LikedItem> findByMemberAndTourItem(Member member, TourItem tourItem);
 
-    Page<LikedItem> findByMember(Member member, Pageable pageable);
-
-    Page<LikedItem> findByMemberAndTourItem_ContentTypeId(Member member, Long categoryCode, Pageable pageable);
+    @Query("SELECT NEW org.tenten.tentenbe.domain.tour.dto.response.TourSimpleResponse(" +
+            "ti.id, " +
+            "ti.contentTypeId, " +
+            "ti.title, " +
+            "CAST(COALESCE(AVG(r.rating), 0) AS DOUBLE), " +
+            "ti.reviewTotalCount, " +
+            "ti.likedTotalCount, " +
+            "COALESCE(CASE WHEN (SELECT COALESCE(COUNT(tli.id), 0) FROM LikedItem tli WHERE tli.tourItem.id = ti.id AND tli.member.id = :memberId) = 1 then true end, false), " +
+            "ti.smallThumbnailUrl, " +
+            "ti.address, " +
+            "ti.longitude, " +
+            "ti.latitude) " +
+            "FROM TourItem ti " +
+            "LEFT OUTER JOIN Review r ON ti.id = r.tourItem.id " +
+            "LEFT OUTER JOIN LikedItem li ON ti.id = li.tourItem.id " +
+            "WHERE li.member.id = :memberId " +
+            "AND (ti.contentTypeId = :category OR :category IS NULL) " +
+            "GROUP BY ti.id")
+    Page<TourSimpleResponse> findByMemberAndCategory(
+            @Param("memberId") Long memberId,
+            @Param("category") Long category,
+            Pageable pageable
+    );
 
     boolean existsByMemberIdAndTourItemId(Long memberId, Long tourItemId);
 }
