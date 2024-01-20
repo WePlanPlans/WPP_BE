8c3fea2281352e5a345592ed11c6842c4eec15fc
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2024-01-21 00:10:34 +0900

    Refactor : 암호화 알고리즘 SHA-256으로 수정

---
+++ src/main/java/org/tenten/tentenbe/global/util/EncryptUtil.java
@@ -0,0 +1,30 @@
+package org.tenten.tentenbe.global.util;
+
+import org.springframework.stereotype.Component;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
+@Component
+public class EncryptUtil {
+
+    public String encrypt(String text)  {
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        md.update(text.getBytes());
+        return bytesToHex(md.digest());
+    }
+
+    private String bytesToHex(byte[] bytes) {
+        StringBuilder builder = new StringBuilder();
+        for (byte b : bytes) {
+            builder.append(String.format("%02x", b));
+        }
+        return builder.toString();
+    }
+}
