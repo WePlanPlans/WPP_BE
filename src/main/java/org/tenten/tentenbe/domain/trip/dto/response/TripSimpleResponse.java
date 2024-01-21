7658c2732ffd279ae6335938746f53f0d994a9e0
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2024-01-21 14:17:32 +0900

    Docs : TripId 스웨거 작성

--- src/main/java/org/tenten/tentenbe/domain/trip/dto/response/TripSimpleResponse.java
+++ src/main/java/org/tenten/tentenbe/domain/trip/dto/response/TripSimpleResponse.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDate;
 
 public record TripSimpleResponse(
-    @Schema(defaultValue = "1")
+    @Schema(defaultValue = "6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b")
     String tripId,
     @Schema(defaultValue = "나의 ~번째 여정")
     String tripName,
